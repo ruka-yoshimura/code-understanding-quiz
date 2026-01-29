afbc82acef9988dbece2e14398446fdfe74958a1
acerola <yoshimuraruka@Acerola-MacBook-Air.local>
2026-01-29 10:44:49 +0900

    feat: ダッシュボード強化とランク別カラーテーマの実装

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -34,4 +34,22 @@ module UserHelper
       "text-yellow-500"
     end
   end
+
+  def user_rank_class(user)
+    level = user.level
+    case level
+    when 1..9
+      "rank-beginner"
+    when 10..19
+      "rank-apprentice"
+    when 20..29
+      "rank-explorer"
+    when 30..39
+      "rank-specialist"
+    when 40..49
+      "rank-master"
+    else
+      "rank-legend"
+    end
+  end
 end
